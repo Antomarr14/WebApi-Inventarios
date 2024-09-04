48ad40d1b0fe87f67346e4d84a3b492da754ad5a
Antomarr14 <diegozetino8@gmail.com>
2024-09-04 14:16:39 -0600

    Agregar archivos de proyecto.

---
+++ WebApi-Inventarios/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApi_Inventarios.Models;
+
+namespace WebApi_Inventarios.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Inventario> inventarios { get; set; }
+    }
+}
---
+++ WebApi-Inventarios/Models/Inventario.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi_Inventarios.Models
+{
+    public class Inventario
+    {
+        public int Id { get; set; }
+        public int Producto_id { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime Fecha_actualizacion { get; set; }
+
+    }
+}
